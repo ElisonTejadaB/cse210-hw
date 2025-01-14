6312919285bc68d463b6e99187d29c370a77d72e
Elisontejadab <elison.tejada.b@gmail.com>
2025-01-13 18:45:54 -0700

    add  generate a random number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,8 +9,12 @@ class Program
         do
         {
 
-            Console.Write("What is the magic number? ");
-            int MagicNumber = int.Parse(Console.ReadLine());
+            //Console.Write("What is the magic number? ");
+            //int MagicNumber = int.Parse(Console.ReadLine());
+
+            Random randomGenerator = new Random();
+            int MagicNumber = randomGenerator.Next(1,100);
+            Console.WriteLine("A magic number between 1 and 100 has been generator.");
 
             int guess = 0;
 
