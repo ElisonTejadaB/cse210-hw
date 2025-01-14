cc9116ed02562675d8ee0647a3e78fe4421c92b0
Elisontejadab <elison.tejada.b@gmail.com>
2025-01-13 17:46:48 -0700

    Add Ask the user for a guess

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int MagicNumber = int.Parse(Console.ReadLine());
+        int guess = 0;
+
+        while (guess != MagicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (MagicNumber > guess)
+            {
+                Console.WriteLine ("Higher");
+
+            }
+            else if (MagicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+
+            }
+            else
+            {
+                Console.WriteLine("Ypu guessed it");
+            }
+        }
     }
 }
