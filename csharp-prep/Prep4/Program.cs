3f760d858875a63323c26186f4b4589aaa3ef42c
Elisontejadab <elison.tejada.b@gmail.com>
2025-01-15 23:21:22 -0700

    add code to prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Elison Tejada Prep4");
+        Console.WriteLine("Enter a list of numbers, Type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        int NewNumbers = -1;
+        while(NewNumbers !=0)
+        {
+            
+            Console.Write("Enter Number ");
+            string InputNumbers = Console.ReadLine();
+            NewNumbers = int.Parse(InputNumbers);
+
+            if (NewNumbers !=0)
+            {
+                numbers.Add(NewNumbers);
+            }
+        } 
+        int sum =0;
+        int number = numbers.Sum();
+        {
+            sum +=number;
+        }
+        Console.WriteLine($"The sum is: {sum}"); 
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}"); 
+
+        int LargestNumber = numbers.Max();
+        Console.WriteLine($"The largest number is: {LargestNumber}");
+
+        numbers.Sort();
+
+        foreach(int NumSort in numbers)
+        Console.WriteLine($"The sorted list is: {NumSort}");
+        
     }
 }
