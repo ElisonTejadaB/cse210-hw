4db5a9023423370ac6b5abe204cf083b243b9526
Elisontejadab <elison.tejada.b@gmail.com>
2025-01-17 00:16:47 -0700

    Add the Code function

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Elison Tejada Prep5 World!");
+        DisplayWelcome();
+        string UserName = PromptUserName();
+        int UserNumber = PromptUserNumber();
+        int Squared = SquaredNumber(UserNumber);
+        DisplayResult(UserName, Squared);
+        
+
+
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string Name = Console.ReadLine();
+        return Name;
+    }
+
+    static int PromptUserNumber()
+
+    {
+        Console.Write("Please enter your favority number: ");
+        int Number = int.Parse(Console.ReadLine());
+        return Number;
+    }
+    static int SquaredNumber(int Number)
+    {
+        int square = Number *Number;
+        return square;
+    }
+
+    static void DisplayResult(string Name, int square)
+    {
+        Console.WriteLine($"{Name}, the square of your number is {square}");
     }
 }
